1690528fbfe4395052d44a8445a768e7261a47c2
InanCelis <celisinan@gmail.com>
2025-11-10 16:14:12 +0800

    Fixed crawling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from './App';
 import '../src/assets/styles/App.scss';
 
+const rootElement = document.getElementById("root");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Check if the page was prerendered by react-snap
+if (rootElement.hasChildNodes()) {
+  // Use hydrate for prerendered content
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  // Use render for client-side only
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
